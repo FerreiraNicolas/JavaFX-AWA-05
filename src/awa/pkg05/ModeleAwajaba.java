689bb9e58e924f7327060751ae4fbd811391bd58
Nicolas <>
2019-12-10 15:12:16 +0100

    AWA-05

---
+++ src/awa/pkg05/ModeleAwajaba.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package awa.pkg05;
+
+/**
+ *
+ * @author etudiant
+ */
+public class ModeleAwajaba {
+    
+    private ModeleAwajaba modele = null;
+
+    public ModeleAwajaba getModele() {
+        return modele;
+    }
+
+    public void setModele(ModeleAwajaba modele) {
+        this.modele = modele;
+    }
+    
+    
+}
